f490d4505d27069fdce77bf26b719da7cdcb0cc5
expertme <mazuka.net@expertme.com.br>
2023-01-23 23:19:53 -0300

    projeto server

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify, { fastify } from "fastify";
+import cors from "@fastify/cors";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
+
+// app.register(cors, {
+//      origin: ['http://localhost:30000']   // Somente esse endereço pode acessar meu bd
+// });
+
+app.register(cors);
+app.register(appRoutes);
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("HTTP Server running");
+});
